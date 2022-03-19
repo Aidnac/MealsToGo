63fccb270e27a9898180a130d5eaa111ead6a23a
Aidnac <calex6658@gmail.com>
2022-03-19 12:56:14 +0300

    Restaurant info setup part 2

--- src/features/restaurant/components/restaurant.info.component.js
+++ src/features/restaurant/components/restaurant.info.component.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import {Text} from 'react-native';
 
-export const RestaurantInfo = () => {
+export const RestaurantInfo = ({restaurant = {}}) => {
+  const {
+    name = 'Some Restaurant',
+    icon,
+    photos = [
+      'https://media.istockphoto.com/photos/cheeseburger-and-french-fries-picture-id153835617?s=612x612',
+    ],
+    address = '100 some random strert',
+    isOpenNow = true,
+    rating = 4,
+    isCLosedTemporarily,
+  } = restaurant;
+
   return <Text>RestaurantInfo text</Text>;
 };
