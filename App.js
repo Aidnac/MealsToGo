e762d0417e9dcdab84d8046b126961ff8b9197c2
Aidnac <calex6658@gmail.com>
2022-03-22 10:46:31 +0300

    Add bottom Tab Navigation

--- App.js
+++ App.js
@@ -2,12 +2,26 @@ import React from 'react';
 import {ThemeProvider} from 'styled-components/native';
 import {theme} from './src/infrastruture/theme';
 import {RestaurantScreen} from './src/features/restaurant/screens/restaurants.screen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Text} from 'react-native';
+
+const Tab = createBottomTabNavigator();
+
+const Maps = () => <Text>Maps</Text>;
+const Settings = () => <Text>Settings</Text>;
 
 export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantScreen />
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Restaurant" component={RestaurantScreen} />
+            <Tab.Screen name="Maps" component={Maps} />
+            <Tab.Screen name="Settings" component={Settings} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
     </>
   );
--- src/features/restaurant/screens/restaurants.screen.js
+++ src/features/restaurant/screens/restaurants.screen.js
@@ -42,7 +42,6 @@ export const RestaurantScreen = () => {
           </Spacer>
         )}
         keyExtractor={item => item.name}
-        contentContainerStyle={{padding: 16}}
       />
     </SafeArea>
   );
