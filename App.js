fbc7c067152b08691bf26563f41ac6e6cd35bed5
Aidnac <calex6658@gmail.com>
2022-03-24 21:04:13 +0300

    Restructing navigation

--- App.js
+++ App.js
@@ -1,42 +1,10 @@
 import React from 'react';
 import {ThemeProvider} from 'styled-components/native';
 import {theme} from './src/infrastruture/theme';
-import {RestaurantsScreen} from './src/features/restaurant/screens/restaurants.screen';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Text} from 'react-native';
-import {SafeArea} from './src/utils/safe-area.component';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import {RestaurantsContextProvider} from './src/services/restaurants/restaurants.context';
 import {LocationContextProvider} from './src/services/location/location.context';
-const Tab = createBottomTabNavigator();
-
-const TAB_ICON = {
-  Restaurants: 'md-restaurant',
-  Map: 'md-map',
-  Settings: 'md-settings',
-};
-
-const createScreenOptions = ({route}) => {
-  const iconName = TAB_ICON[route.name];
-  return {
-    tabBarIcon: ({size, color}) => (
-      <Ionicons name={iconName} size={size} color={color} />
-    ),
-  };
-};
-
-const Maps = () => (
-  <SafeArea>
-    <Text>Maps</Text>
-  </SafeArea>
-);
-const Settings = () => (
-  <SafeArea>
-    <Text>Settings</Text>
-  </SafeArea>
-);
+import {Navigation} from './src/infrastruture/navigation';
 
 export default function App() {
   return (
@@ -44,18 +12,7 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <LocationContextProvider>
           <RestaurantsContextProvider>
-            <NavigationContainer>
-              <Tab.Navigator
-                screenOptions={createScreenOptions}
-                tabBarOptions={{
-                  activeTintColor: 'tomato',
-                  inactiveTintColor: 'gray',
-                }}>
-                <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
-                <Tab.Screen name="Map" component={Maps} />
-                <Tab.Screen name="Settings" component={Settings} />
-              </Tab.Navigator>
-            </NavigationContainer>
+            <Navigation />
           </RestaurantsContextProvider>
         </LocationContextProvider>
       </ThemeProvider>
