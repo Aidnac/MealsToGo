a5dcdc8f82c4eb49be607d09dd1807e99952c5c7
Aidnac <calex6658@gmail.com>
2022-03-27 22:23:47 +0300

    Add image on map marker and navigation to restaturant detail

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -3,6 +3,7 @@ import MapView from 'react-native-maps';
 import styled from 'styled-components/native';
 import {LocationContext} from '../../../services/location/location.context';
 import {RestaurantsContext} from '../../../services/restaurants/restaurants.context';
+import {MapCallout} from '../component/map-callout.component';
 
 import {Search} from '../component/search.component';
 
@@ -11,7 +12,7 @@ const Map = styled(MapView)`
   width: 100%;
 `;
 
-export const MapScreen = () => {
+export const MapScreen = ({navigation}) => {
   const {location} = useContext(LocationContext);
   const {restaurants = []} = useContext(RestaurantsContext);
 
@@ -44,8 +45,16 @@ export const MapScreen = () => {
               coordinate={{
                 latitude: restaurant.geometry.location.lat,
                 longitude: restaurant.geometry.location.lng,
-              }}
-            />
+              }}>
+              <MapView.Callout
+                onPress={() =>
+                  navigation.navigate('RestaurantDetail', {
+                    restaurant,
+                  })
+                }>
+                <MapCallout restaurant={restaurant} />
+              </MapView.Callout>
+            </MapView.Marker>
           );
         })}
       </Map>
