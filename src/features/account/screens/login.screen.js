c8938c857de0e87aa176185a6e746d2d59b1eb0c
Aidnac <calex6658@gmail.com>
2022-04-03 19:42:06 +0300

    Some Clean up

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -5,18 +5,21 @@ import {
   AccountContainer,
   AuthButton,
   AuthInput,
+  Title,
+  ErrorContainer,
 } from '../components/account.styles';
 import {Text} from '../../../components/typography/text.component';
 import {Spacer} from '../../restaurant/components/spacer.component';
 import {AuthenticationContext} from '../../../services/authentication/authentication.context';
 
-export const LoginScreen = () => {
+export const LoginScreen = ({navigation}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const {onLogin, error} = useContext(AuthenticationContext);
   return (
     <AccountBackground>
       <AccountCover />
+      <Title>Meals To Go</Title>
       <AccountContainer>
         <AuthInput
           label="E-mail"
@@ -38,9 +41,9 @@ export const LoginScreen = () => {
           />
         </Spacer>
         {error && (
-          <Spacer size="large">
+          <ErrorContainer size="large">
             <Text variant="error">{error}</Text>
-          </Spacer>
+          </ErrorContainer>
         )}
         <Spacer size="large">
           <AuthButton
@@ -51,6 +54,11 @@ export const LoginScreen = () => {
           </AuthButton>
         </Spacer>
       </AccountContainer>
+      <Spacer size="large">
+        <AuthButton mode="contained" onPress={() => navigation.goBack()}>
+          Back
+        </AuthButton>
+      </Spacer>
     </AccountBackground>
   );
 };
