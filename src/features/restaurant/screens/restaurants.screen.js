9a441b4917f96bb8d484dee100325ccbeee2ad6f
Aidnac <calex6658@gmail.com>
2022-04-04 21:46:18 +0300

    Favouritescreen setup

--- src/features/restaurant/screens/restaurants.screen.js
+++ src/features/restaurant/screens/restaurants.screen.js
@@ -11,12 +11,7 @@ import {SafeArea} from '../../../utils/safe-area.component';
 import {Spacer} from '../components/spacer.component';
 import {FavouritesBar} from '../../../components/favourites/favourites-bar.component';
 import {FavouritesContext} from '../../../services/favourites/favourites.context';
-
-const RestaurantList = styled(FlatList).attrs({
-  contentContainerStyle: {
-    padding: 16,
-  },
-})``;
+import {RestaurantList} from '../components/restaurant-list.styles';
 
 const Loading = styled(ActivityIndicator)`
   margin-left: -25px;
